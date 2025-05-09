72746c909e16d5d0631dab982e5dfe1639b08675
wayne-617 <wgl91603@uga.edu>
2025-05-09 15:14:43 -0400

    Fetch data for testing

--- frontend/tests/performance/PerformanceTestComponent.tsx
+++ frontend/tests/performance/PerformanceTestComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import StockList from "../../src/components/StockList";
 import StockListIndividual from "../../src/components/StockList.individual";
 
@@ -10,11 +10,39 @@ interface Stock {
   }
 
 const PerformanceTest: React.FC = () => {
-  const [stocks] = useState<Stock[]>();
+  const [stocks, setStocks] = useState<Stock[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/securities", {
+            method: "GET",
+            credentials: "include",
+            })
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const data = await response.json();
+
+            setStocks(data);
+        } catch (error) {
+            console.error("Error fetching stocks:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchData();
+  }, []);
   
   const handleAction = (symbol: string) => {
     console.log(`Action for ${symbol}`);
   };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
