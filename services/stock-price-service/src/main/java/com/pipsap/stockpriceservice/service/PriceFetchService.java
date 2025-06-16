da999258b956d3462fc824fda333a88547ba57e7
firasastwani <firasastwani1@gmail.com>
2025-06-16 13:05:26 -0400

    hiding API keys

--- services/stock-price-service/src/main/java/com/pipsap/stockpriceservice/service/PriceFetchService.java
+++ services/stock-price-service/src/main/java/com/pipsap/stockpriceservice/service/PriceFetchService.java
@@ -1,5 +1,6 @@
 package com.pipsap.stockpriceservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.pipsap.stockpriceservice.model.FinnhubQuote;
@@ -7,7 +8,9 @@ import com.pipsap.stockpriceservice.model.FinnhubQuote;
 @Service
 public class PriceFetchService {
 
-    private final String API_KEY = "d162ko1r01qhsocmb2g0d162ko1r01qhsocmb2gg";
+    @Value("${finnhub.api-key}")
+    private String apiKey;
+    
     private final RestTemplate restTemplate;
     private final String FINNHUB_BASE_URL = "https://finnhub.io/api/v1/quote";
 
@@ -17,7 +20,7 @@ public class PriceFetchService {
     
     public double fetchPrice(String symbol) {
         try {
-            String url = String.format("%s?symbol=%s&token=%s", FINNHUB_BASE_URL, symbol, API_KEY);
+            String url = String.format("%s?symbol=%s&token=%s", FINNHUB_BASE_URL, symbol, apiKey);
             FinnhubQuote quote = restTemplate.getForObject(url, FinnhubQuote.class);
             return quote != null ? quote.getCurrentPrice() : 0.0;
         } catch (Exception e) {
